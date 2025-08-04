37a5c2f4fb1fae0f65e0f4d1b641d8c72a8aac21
jishnuprasad <jishnuprasadtctc@gmail.com>
2025-08-05 00:07:17 +0530

    update

--- newborncare/settings.py
+++ newborncare/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -23,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-8coyf7fjq%qq=7l)v#ieicsu!io5glvc82-ij$e*c*bo=9*!-$'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -120,6 +121,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 STATICFILES_DIRS = [
     BASE_DIR / "static",  # Base directory for static files
@@ -130,7 +132,8 @@ STATICFILES_DIRS = [
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 
 
@@ -140,3 +143,9 @@ EMAIL_USE_TLS = True
 EMAIL_PORT = 587
 EMAIL_HOST_USER = 'jishnuprasadtctc@gmail.com'
 EMAIL_HOST_PASSWORD = 'wuad wwtz vdhh snsy'
+
+
+import dj_database_url
+
+
+DATABASES['default'] = dj_database_url.config(conn_max_age=600)
